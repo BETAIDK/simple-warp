0beb1b39da784dad3c1d9b50b07b2be099f13e5b
idan <idankob@gmail.com>
2023-04-29 11:21:45 +0300

    Added: - setWarp command

---
+++ src/main/java/dev/tp/commands/SetWarp.java
@@ -0,0 +1,44 @@
+package dev.tp.commands;
+
+import dev.tp.TpPluginLoader;
+import dev.tp.sys.WarpManager;
+import dev.tp.utils.OfflineLocation;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class SetWarp implements CommandExecutor {
+
+    private final TpPluginLoader plugin;
+
+    public SetWarp(TpPluginLoader plugin) {
+        this.plugin = plugin;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (!(sender instanceof Player player)) {
+            Bukkit.getLogger().severe("Only player allowed to use commands");
+            return true;
+        }
+
+        if (!player.hasPermission("plugin.admin.setwarp")) {
+            sender.sendMessage("§cYou don't have permission to use this command");
+            return false;
+        }
+
+        if (args.length != 1) {
+            sender.sendMessage("§cUsage: /setwarp <name>");
+            return false;
+        }
+
+        WarpManager manager = plugin.getWarpManager();
+        String warpName = args[0];
+
+        manager.addWarp(warpName, OfflineLocation.from(player.getLocation()));
+        player.sendMessage("Warp has been set by the name:" + warpName);
+        return true;
+    }
+}
